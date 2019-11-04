0788fc42bf82d1a3dc89c98fc04beabe27003c16
JBonifay <joffrey.bonifay@alcineo.com>
2019-11-04 14:11:00 +0100

    Add spring jpa fields in livre object

--- livres/src/main/java/com/openclassrooms/livres/domain/Livre.java
+++ livres/src/main/java/com/openclassrooms/livres/domain/Livre.java
@@ -1,13 +1,21 @@
 package com.openclassrooms.livres.domain;
 
 import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Livre {
 
+    @Id
+    @Column(name = "id")
     private String name;
+
+    @Column(name = "author")
     private String author;
+
+    @Column(name = "release_date")
     private Date   releaseDate;
 
 }
