c32c1ff2aa27377e386bd4375f5361d815aaf8e4
JBonifay <joffrey.bonifay@alcineo.com>
2019-11-13 13:50:20 +0100

    Add config server / spring cloud

---
+++ config-server/src/test/java/com/openclassrooms/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
