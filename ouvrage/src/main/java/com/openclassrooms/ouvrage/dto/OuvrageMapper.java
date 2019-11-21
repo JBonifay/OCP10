796668e557a4b1fe7692d4142fc24ae1ed11c4c5
JBonifay <joffreybonifay83@gmail.com>
2019-11-21 13:03:15 +0100

    Improve mapstruck interface

--- ouvrage/src/main/java/com/openclassrooms/ouvrage/dto/OuvrageMapper.java
+++ ouvrage/src/main/java/com/openclassrooms/ouvrage/dto/OuvrageMapper.java
@@ -2,11 +2,17 @@ package com.openclassrooms.ouvrage.dto;
 
 import com.openclassrooms.ouvrage.model.Ouvrage;
 import java.util.List;
+import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface OuvrageMapper {
 
+    @Mapping(source = "stock.quantite", target = "quantite")
+    OuvrageStockDto toOuvrageStockDto(Ouvrage ouvrage);
+
+    @InheritConfiguration
     List<OuvrageStockDto> toProductDTOs(List<Ouvrage> ouvrageList);
 
     OuvrageDto toProductDTO(Ouvrage ouvrage);
