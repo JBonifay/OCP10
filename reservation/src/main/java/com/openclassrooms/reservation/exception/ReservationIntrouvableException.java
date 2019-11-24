bff028d322d0b32155e6f221d3ab3e4adfa01fcc
JBonifay <joffreybonifay83@gmail.com>
2019-11-24 16:40:52 +0100

    Add reservation not found exception

--- reservation/src/main/java/com/openclassrooms/reservation/exception/ReservationIntrouvableException.java
+++ reservation/src/main/java/com/openclassrooms/reservation/exception/ReservationIntrouvableException.java
@@ -1,8 +1,13 @@
 package com.openclassrooms.reservation.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ReservationIntrouvableException extends RuntimeException {
 
-    public ReservationIntrouvableException(String s) {
+    public ReservationIntrouvableException(String error) {
+        super(error);
     }
 
 }
