8cc97d95b8f1bb145082965a8f8cb9ad8f50ab3a
JBonifay <joffreybonifay83@gmail.com>
2019-11-25 12:59:46 +0100

    Add eureka edge service

---
+++ eureka/src/test/java/com/openclassrooms/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
