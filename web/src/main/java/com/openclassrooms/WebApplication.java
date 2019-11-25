c0c0b74566e2d3332d00be33be53652c39ec64c9
JBonifay <joffreybonifay83@gmail.com>
2019-11-25 13:11:12 +0100

    Add eureka dependency

--- web/src/main/java/com/openclassrooms/WebApplication.java
+++ web/src/main/java/com/openclassrooms/WebApplication.java
@@ -2,10 +2,12 @@ package com.openclassrooms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.openclassrooms")
+@EnableDiscoveryClient
 public class WebApplication {
 
 	public static void main(String[] args) {
