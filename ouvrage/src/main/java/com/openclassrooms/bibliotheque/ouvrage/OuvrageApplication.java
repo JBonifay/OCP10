0f76154c9365b0f5d70ca5a95c43cfb7a08c32ed
JBonifay <joffreybonifay83@gmail.com>
2019-11-29 11:48:17 +0100

    Remove duplicated dependency
    Took 11 minutes

--- ouvrage/src/main/java/com/openclassrooms/bibliotheque/ouvrage/OuvrageApplication.java
+++ ouvrage/src/main/java/com/openclassrooms/bibliotheque/ouvrage/OuvrageApplication.java
@@ -3,7 +3,6 @@ package com.openclassrooms.bibliotheque.ouvrage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
--- utilisateur/src/main/java/com/openclassrooms/bibliotheque/utilisateur/UtilisateurApplication.java
+++ utilisateur/src/main/java/com/openclassrooms/bibliotheque/utilisateur/UtilisateurApplication.java
@@ -3,7 +3,7 @@ package com.openclassrooms.bibliotheque.utilisateur;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
