865e34181888a3da712622d3060e7ac17e914e1c
JBonifay <joffreybonifay83@gmail.com>
2019-11-29 13:01:21 +0100

    Remove spring security due to auth error in post request
    Took 1 hour 2 minutes

--- web/src/main/java/com/openclassrooms/bibliotheque/web/configuration/FeignConfig.java
+++ web/src/main/java/com/openclassrooms/bibliotheque/web/configuration/FeignConfig.java
@@ -1,15 +1,13 @@
 package com.openclassrooms.bibliotheque.web.configuration;
 
-import feign.auth.BasicAuthRequestInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
 
-    @Bean
-    public BasicAuthRequestInterceptor mBasicAuthRequestInterceptor(){
-        return  new BasicAuthRequestInterceptor("bibliotheque-zuul", "zuulbibliotheque");
-    }
+    // @Bean
+    // public BasicAuthRequestInterceptor mBasicAuthRequestInterceptor() {
+    //     return new BasicAuthRequestInterceptor("bibliotheque-zuul", "zuulbibliotheque");
+    // }
 
 }
