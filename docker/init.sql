67b9ab6c8078c15f91affdec614e91ae09f47a12
JBonifay <joffreybonifay83@gmail.com>
2019-12-06 12:39:32 +0100

    Remove table stock and merge it with ouvrageé

--- docker/init.sql
+++ docker/init.sql
@@ -1,10 +1,8 @@
 CREATE DATABASE IF NOT EXISTS ouvrage;
 CREATE DATABASE IF NOT EXISTS reservation;
 CREATE DATABASE IF NOT EXISTS utilisateur;
-CREATE DATABASE IF NOT EXISTS stock;
 
 CREATE USER 'bibliotheque'@'%' IDENTIFIED BY 'rootbibliotheque';
 GRANT ALL PRIVILEGES ON ouvrage.* TO 'bibliotheque'@'%';
 GRANT ALL PRIVILEGES ON reservation.* TO 'bibliotheque'@'%';
 GRANT ALL PRIVILEGES ON utilisateur.* TO 'bibliotheque'@'%';
-GRANT ALL PRIVILEGES ON stock.* TO 'bibliotheque'@'%';
