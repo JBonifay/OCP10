e9d7a8e163fd458c8a533e089a94dade6bd21b46
JBonifay <joffreybonifay83@gmail.com>
2019-12-13 10:10:47 +0100

    Delete unused config class
    Took 10 minutes

--- web/src/main/java/com/openclassrooms/bibliotheque/web/config/SecurityConfig.java
+++ web/src/main/java/com/openclassrooms/bibliotheque/web/config/SecurityConfig.java
@@ -1,56 +1,53 @@
 package com.openclassrooms.bibliotheque.web.config;
 
-import com.openclassrooms.bibliotheque.web.service.AppAuthProvider;
 import com.openclassrooms.bibliotheque.web.service.UserService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserService userDetailsService;
+  private final UserService userDetailsService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-    }
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService);
+  }
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .logout()
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID");
+  @Override
+  protected void configure(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.authorizeRequests()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin()
+        .and()
+        .logout()
+        .invalidateHttpSession(true)
+        .deleteCookies("JSESSIONID");
 
-        httpSecurity.csrf().disable();
+    httpSecurity.csrf().disable();
 
-    }
+  }
 
-    @Bean
-    public AuthenticationProvider getProvider() {
-        AppAuthProvider provider = new AppAuthProvider();
-        provider.setUserDetailsService(userDetailsService);
-        return provider;
-    }
-
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
 }
