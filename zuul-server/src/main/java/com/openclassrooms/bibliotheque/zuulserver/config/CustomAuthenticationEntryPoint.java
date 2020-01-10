935e61d118774c4f3f28a3cecb61d41915dc5bb7
JBonifay <joffreybonifay83@gmail.com>
2020-01-10 12:29:57 +0100

    Set error
    Took 1 hour 1 minute

--- zuul-server/src/main/java/com/openclassrooms/bibliotheque/zuulserver/config/CustomAuthenticationEntryPoint.java
+++ zuul-server/src/main/java/com/openclassrooms/bibliotheque/zuulserver/config/CustomAuthenticationEntryPoint.java
@@ -1,13 +1,11 @@
 package com.openclassrooms.bibliotheque.zuulserver.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.openclassrooms.bibliotheque.zuulserver.exception.AuthenticationFailureException;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -17,19 +15,12 @@ import org.springframework.stereotype.Component;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException {
-        AuthenticationFailureException error = new AuthenticationFailureException(e.getMessage());
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+            throws IOException, ServletException {
 
-        try {
-            String json = new ObjectMapper().writeValueAsString(error);
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setCharacterEncoding(StandardCharsets.UTF_8.toString());
-            response.getWriter().write(json);
-
-        } catch (Exception e1) {
-            log.error(e1.getMessage());
-        }
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("HTTP Status 401 - " + authEx.getMessage());
 
     }
 }
