54c7a6029b3ab44ad04fbfc555ae0a779154ed6c
JBonifay <joffreybonifay83@gmail.com>
2020-01-14 13:51:26 +0100

    Push with error

--- web/src/main/java/com/openclassrooms/bibliotheque/web/WebApplication.java
+++ web/src/main/java/com/openclassrooms/bibliotheque/web/WebApplication.java
@@ -2,11 +2,12 @@ package com.openclassrooms.bibliotheque.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class WebApplication {
     
     public static void main(String[] args) {
