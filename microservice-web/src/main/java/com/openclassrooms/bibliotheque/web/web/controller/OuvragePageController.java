8464bac2ef50ddad8fd088ed221e7b057c51ff84
JBonifay <joffreybonifay83@gmail.com>
2020-01-22 13:18:59 +0100

    Set error for pagination > maximum pages

--- microservice-web/src/main/java/com/openclassrooms/bibliotheque/web/web/controller/OuvragePageController.java
+++ microservice-web/src/main/java/com/openclassrooms/bibliotheque/web/web/controller/OuvragePageController.java
@@ -5,10 +5,11 @@ import com.openclassrooms.bibliotheque.web.beans.ouvrage.OuvrageStockBean;
 import com.openclassrooms.bibliotheque.web.proxies.OuvrageProxy;
 import com.openclassrooms.bibliotheque.web.proxies.RestPageImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,11 +21,15 @@ public class OuvragePageController {
     
     private final OuvrageProxy ouvrageProxy;
     
+    @SneakyThrows
     @GetMapping({"/listedesouvrages", "/"})
     public ModelAndView getOuvragesPage(Pageable pageable) {
         ModelAndView ouvrages = new ModelAndView("listedesouvrages");
         ResponseEntity<RestPageImpl<OuvrageStockBean>> ouvragePage = ouvrageProxy.getAllOuvrageListByPage(pageable);
         if (ouvragePage.getStatusCode() == HttpStatus.OK) {
+            if (ouvragePage.getBody().getPageable().getPageNumber() > ouvragePage.getBody().getTotalPages()) {
+                throw new NotFoundException();
+            }
             ouvrages.addObject("ouvrages", ouvragePage.getBody().getContent());
             ouvrages.addObject("pageable", ouvragePage.getBody());
         }
