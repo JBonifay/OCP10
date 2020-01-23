0975b4e048add38e2cea81750b9922ec7efdcb05
JBonifay <joffreybonifay83@gmail.com>
2020-01-23 13:08:37 +0100

    microservice-utilisateur -> Implement better error statement with ResponseStatusException

--- microservice-utilisateur/src/main/java/com/openclassrooms/utilisateur/rest/UtilisateurController.java
+++ microservice-utilisateur/src/main/java/com/openclassrooms/utilisateur/rest/UtilisateurController.java
@@ -3,11 +3,13 @@ package com.openclassrooms.utilisateur.rest;
 import com.openclassrooms.utilisateur.model.Utilisateur;
 import com.openclassrooms.utilisateur.repository.UtilisateurRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,12 +19,20 @@ public class UtilisateurController {
 
     @GetMapping("/utilisateur")
     public ResponseEntity<Utilisateur> getUtilisateurById(@RequestParam String utilisateurId) {
-        return ResponseEntity.of(utilisateurRepository.findById(Integer.valueOf(utilisateurId)));
+        Utilisateur utilisateur = utilisateurRepository.findById(Integer.valueOf(utilisateurId)).orElse(null);
+        if (utilisateur == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        return ResponseEntity.ok(utilisateur);
     }
 
     @GetMapping("/utilisateur/{email}")
     public ResponseEntity<Utilisateur> getUtilisateurByEmail(@PathVariable String email) {
-        return ResponseEntity.of(utilisateurRepository.findUtilisateurByEmailIgnoreCase(email));
+        Utilisateur utilisateur = utilisateurRepository.findUtilisateurByEmailIgnoreCase(email).orElse(null);
+        if (utilisateur == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        return ResponseEntity.ok(utilisateur);
     }
 
 
