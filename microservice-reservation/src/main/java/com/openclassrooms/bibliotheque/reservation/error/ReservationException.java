41af250e1fd68d43c157bf49f6c7da443920aa48
JBonifay <joffrey.bonifay@alcineo.com>
2020-05-11 13:30:52 +0200

    Add exception for reservatiopn extension

--- microservice-reservation/src/main/java/com/openclassrooms/bibliotheque/reservation/error/ReservationException.java
+++ microservice-reservation/src/main/java/com/openclassrooms/bibliotheque/reservation/error/ReservationException.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.bibliotheque.reservation.exception;
+package com.openclassrooms.bibliotheque.reservation.error;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
