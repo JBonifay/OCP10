a3e0c758295362221ddbb130075dd2f10cf108e5
JBonifay <joffrey.bonifay@alcineo.com>
2020-05-15 12:02:07 +0200

    Change Ouvrage Date to sql.Date, add one test

--- microservice-ouvrage/src/test/java/com/openclassrooms/bibliotheque/ouvrage/OuvrageServiceTest.java
+++ microservice-ouvrage/src/test/java/com/openclassrooms/bibliotheque/ouvrage/OuvrageServiceTest.java
@@ -1,8 +1,15 @@
 package com.openclassrooms.bibliotheque.ouvrage;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.openclassrooms.bibliotheque.ouvrage.model.Ouvrage;
+import com.openclassrooms.bibliotheque.ouvrage.model.Stock;
 import com.openclassrooms.bibliotheque.ouvrage.service.OuvrageService;
-import java.sql.SQLException;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +22,28 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 public class OuvrageServiceTest {
 
     @Autowired
-    private OuvrageService ouvrageService;
+    private             OuvrageService ouvrageService;
+    public static final Ouvrage        OUVRAGE = new Ouvrage();
+
+    @BeforeAll
+    static void init() throws ParseException {
+        OUVRAGE.setOuvrageId(1);
+        OUVRAGE.setName("Acid House, The");
+        OUVRAGE.setAuthor("Geneva Wetherby");
+
+        // Format date into output format
+        OUVRAGE.setReleaseDate(new Date(new SimpleDateFormat("yyyy-MM-dd").parse("2019-10-28").getTime()));
+        OUVRAGE.setSummary(
+                "In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat. Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat. Nulla nisl. Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst.");
+        OUVRAGE.setEditor("unicef.org");
+        OUVRAGE.setNumberOfPages(982);
+        OUVRAGE.setNotation(4);
+        OUVRAGE.setStock(new Stock(1, 1, 2));
+    }
 
     @Test
-    public void test() throws SQLException {
-
+    public void findByOuvrageIdAndAssertIsEqualTest() {
+        assertThat(ouvrageService.findOuvrageById(1)).isEqualTo(OUVRAGE);
     }
 
 
