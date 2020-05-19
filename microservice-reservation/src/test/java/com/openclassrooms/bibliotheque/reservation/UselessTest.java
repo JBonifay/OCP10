8e4ca7977c716dd25600dba2d59af3931d097b75
JBonifay <joffrey.bonifay@alcineo.com>
2020-05-19 14:09:57 +0200

    Setup JaCoCo

---
+++ microservice-reservation/src/test/java/com/openclassrooms/bibliotheque/reservation/UselessTest.java
@@ -0,0 +1,15 @@
+package com.openclassrooms.bibliotheque.reservation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class UselessTest {
+
+    @Test
+    public void testeste() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+
+
+}
